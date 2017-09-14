c512c7a7495e9b56642e5b5153d1be911b7772d6
Assani <as.elhadaoui@gmail.com>
2017-09-14 10:14:04 +0200

    Page login

--- src/MainBundle/Controller/DefaultController.php
+++ src/MainBundle/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace MainBundle\Controller;
 
 use MainBundle\Entity\Joueurs;
+use MainBundle\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -14,6 +15,7 @@ class DefaultController extends Controller
         $em = $this->getDoctrine()->getManager();
         $joueurs = $em->getRepository('MainBundle:Joueurs')->findAll();
         $user = $this->getUser();
+
 
         return $this->render('MainBundle:Default:index.html.twig', array(
             'joueurs'=>$joueurs,
@@ -30,10 +32,12 @@ class DefaultController extends Controller
 
         //On récupère l'objet du joueur
         $joueurs = $em->getRepository('MainBundle:Joueurs')->findOneById($id);
+
         //On le set
         $User->setJoueur($joueurs);
+        $joueurs->setVoix($joueurs->getVoix() + 1);
 
-        $em->persist($User);
+        $em->persist($User, $joueurs);
         $em->flush();
 
         return $this->render('@Main/Default/redirect.html.twig');
